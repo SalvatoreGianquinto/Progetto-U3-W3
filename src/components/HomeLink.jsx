3df130096e3befdefcfe942e6f28a48e2afcf99e
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-04-18 16:36:52 +0200

    piccole aggiunte

--- src/components/HomeLink.jsx
+++ src/components/HomeLink.jsx
@@ -7,59 +7,82 @@ const HomeLink = function () {
       <Row className="g-2">
         <Col xs={12} lg={4}>
           <Card className="bg-dark p-2">
-            <Card.Body className="text-danger fs-4 fs-4">
+            <Card.Body className="text-danger fs-4 d-flex justify-content-between">
               Esplora per genere
+              <i className="bi bi-chevron-right"></i>
             </Card.Body>
           </Card>
         </Col>
 
         <Col xs={12} lg={4}>
           <Card className="bg-dark p-2 fs-4">
-            <Card.Body className="text-danger">Decenni</Card.Body>
+            <Card.Body className="text-danger d-flex justify-content-between">
+              Decenni
+              <i className="bi bi-chevron-right"></i>
+            </Card.Body>
           </Card>
         </Col>
 
         <Col xs={12} lg={4}>
           <Card className="bg-dark p-2 fs-4">
-            <Card.Body className="text-danger">
+            <Card.Body className="text-danger d-flex justify-content-between">
               Attività e stati d'animo
+              <i className="bi bi-chevron-right"></i>
             </Card.Body>
           </Card>
         </Col>
 
         <Col xs={12} lg={4}>
           <Card className="bg-dark p-2 fs-4">
-            <Card.Body className="text-danger">Worldwide</Card.Body>
+            <Card.Body className="text-danger d-flex justify-content-between">
+              Worldwide
+              <i className="bi bi-chevron-right"></i>
+            </Card.Body>
           </Card>
         </Col>
 
         <Col xs={12} lg={4}>
           <Card className="bg-dark p-2 fs-4">
-            <Card.Body className="text-danger">Classifiche</Card.Body>
+            <Card.Body className="text-danger d-flex justify-content-between">
+              Classifiche
+              <i className="bi bi-chevron-right"></i>
+            </Card.Body>
           </Card>
         </Col>
 
         <Col xs={12} lg={4}>
           <Card className="bg-dark p-2 fs-4">
-            <Card.Body className="text-danger">Audio Spaziale</Card.Body>
+            <Card.Body className="text-danger d-flex justify-content-between">
+              Audio Spaziale
+              <i className="bi bi-chevron-right"></i>
+            </Card.Body>
           </Card>
         </Col>
 
         <Col xs={12} lg={4}>
           <Card className="bg-dark p-2 fs-4">
-            <Card.Body className="text-danger">Video Musicali</Card.Body>
+            <Card.Body className="text-danger d-flex justify-content-between">
+              Video Musicali
+              <i className="bi bi-chevron-right"></i>
+            </Card.Body>
           </Card>
         </Col>
 
         <Col xs={12} lg={4}>
           <Card className="bg-dark p-2 fs-4">
-            <Card.Body className="text-danger">Nuovi Artisti</Card.Body>
+            <Card.Body className="text-danger d-flex justify-content-between">
+              Nuovi Artisti
+              <i className="bi bi-chevron-right"></i>
+            </Card.Body>
           </Card>
         </Col>
 
         <Col xs={12} lg={4}>
           <Card className="bg-dark p-2 fs-4">
-            <Card.Body className="text-danger">Hit del passato</Card.Body>
+            <Card.Body className="text-danger d-flex justify-content-between">
+              Hit del passato
+              <i className="bi bi-chevron-right"></i>
+            </Card.Body>
           </Card>
         </Col>
       </Row>
